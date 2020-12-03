0f052b912f1c8dfeba49d5bc750ae3f3724716bf
Utsah Bharali <63600897+UtsahBharali@users.noreply.github.com>
2020-12-03 22:54:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import ReadStoryScreen from './screens/ReadStoryScreen';
+import WriteStoryScreen from './screens/WriteStoryScreen';
+
+export default class App extends React.Component {
+  render() {
+    return(
+       <AppContainer />
+    ) 
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+    WriteStory: { screen: WriteStoryScreen },
+    ReadStory: { screen: ReadStoryScreen },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({}) => {
+        const routeName = navigation.state.routeName;
+        if (routeName === 'ReadStory') {
+          return (
+            <Image
+              source={require('./assets/read.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          );
+        } else if (routeName === 'WriteStory') {
+          return (
+            <Image
+              source={require('./assets/write.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          );
+        }
+      },
+    }),
+  }
+);
+
+const AppContainer = createAppContainer(TabNavigator);
